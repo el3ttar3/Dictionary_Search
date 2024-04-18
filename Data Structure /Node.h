8a836f51a2fa4930b1317c8906d61360ac85c639
Adham Ashraf Eltholth <147609804+aadhamashraf@users.noreply.github.com>
2024-04-18 17:11:58 +0200

    Create Node.h

---
+++ Data Structure /Node.h
@@ -0,0 +1,24 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <iostream>
+
+struct Node {
+    int val;      // Value stored in the node
+    Node* next;  // Pointer to the next node
+    Node* prev;  // Pointer to the previous node
+
+    Node(int value);
+
+    void forward_while_print(Node* head);
+    void forward_for_print(Node* head);
+    void forward_rec_print(Node* head);
+
+    void backward_while_print(Node* start, Node* head);
+    void backward_for_print(Node* start, Node* head);
+    void backward_rec_print(Node* goal);
+
+    int search(int val, Node* head);
+};
+
+#endif 
