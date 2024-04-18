462a35beb4dc606bb57a44f85f8280b9369a84c4
Adham Ashraf Eltholth <147609804+aadhamashraf@users.noreply.github.com>
2024-04-18 14:26:20 +0200

    Update Vector.h

--- Data Structure /Vector.h
+++ Data Structure /Vector.h
@@ -1,1 +1,24 @@
-#include<iostream>
+#ifndef VECTOR_H
+#define VECTOR_H
+
+class Vector {
+private:
+    int size{ -1 };
+    int* arr{ nullptr };
+    int capacity{ -1 };
+
+public:
+    Vector(int s);
+    void printVector();
+    void find(int value);
+    void addSize();
+    void pushbackV1(int val);
+    void pushbackV2(int val);
+    int getValue(int idx);
+    int getFront();
+    int getBack();
+    void insert_at(int idx, int value);
+    ~Vector();
+};
+
+#endif // VECTOR_H
