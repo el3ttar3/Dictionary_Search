353276e5f2ca75848bfd959bebe21558e2ae15a9
Adham Ashraf Eltholth <147609804+aadhamashraf@users.noreply.github.com>
2024-04-20 12:29:28 +0200

    Update LinkedList.cpp

--- Data Structure /LinkedList.cpp
+++ Data Structure /LinkedList.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+#include"Node.h"
 
+class LinkedList{
+
+private:
+  Node*head;
+
+public:
+
+  LinkedList();
+  void insert_end(const std::string sample);
+  void search(const std::string &prefix);
+  void start_with(const char keychar );
+
+};
